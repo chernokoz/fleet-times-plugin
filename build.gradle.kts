ac19735f8e4817bdb994d95ba2327f9bbddb5ef7
chernokoz <chernokoz@hotmail.com>
2023-08-21 13:07:33 +0300

    update the versions to be up-to-date and adjust the code for API changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,21 @@ repositories {
 plugins {
     kotlin("jvm") version "1.8.0"
     `kotlin-dsl`
-    id("org.jetbrains.fleet-plugin") version "0.1.30"
+    id("org.jetbrains.fleet-plugin") version "0.2.63"
 }
 
 version = "0.1.0"
+dependencies {
+    implementation("org.jetbrains:annotations:24.0.0")
+}
 
 fleet {
-    fleetVersion.set("1.17.20")
-    useNightlyBuilds.set(true)
+    fleetVersion.set("1.23.75")
 
     // presentation
-    vendor.set("Sergey Ignatov")
-    readableName.set("Fleet Sample Plugin")
-    descriptor.set("A description for sample plugin")
+    vendor.set("Aleksandr Chernokoz")
+    readableName.set("Fleet Times Plugin")
+    descriptor.set("A simple plugin adding an action to open the website of The Times newspaper.")
 
     workspace {
         // workspace dependencies
